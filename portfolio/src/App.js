5eda18a2c0cb594f134a05cb8321895ddb861491
Mic Doodle <mcdoodle@Mics-MBP.fios-router.home>
2020-12-21 00:19:08 -0500

    spinup react app

---
+++ portfolio/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './components/navigation'
+
+
+
+function App() {
+  return (
+    <>
+      <Navigation />
+    </>
+  );
+}
+
+
+
+export default App;
---
+++ portfolio/src/components/navigation.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navigation() {
+
+    return (
+        <header>
+            <div>
+                <h1>Jaime Ferson</h1>
+            </div>
+
+
+        </header>
+    )
+}
+
+export default Navigation
